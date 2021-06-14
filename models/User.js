a378722847fe52cf86fbd70fdd051d27870883ad
abu sayeed <sayeed0209@gmail.com>
2021-06-14 13:37:44 +0200

    user model

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+	firstName: String,
+	lastName: String,
+	email: { type: String, required: true, unique: true },
+	permissionLevel: Number,
+});
+
+module.exports = mongoose.model("User", UserSchema);
