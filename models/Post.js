1ed91e799dcd26ed4b51fc5b0b7accdc77d9cf0c
abu sayeed <sayeed0209@gmail.com>
2021-06-17 11:50:28 +0200

    added trim to be true on the post model in-order to avoid empty space

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const PostSchema = new Schema({
 	author: String,
-	title: String,
+	title: { type: String, trim: true },
 });
 
 module.exports = mongoose.model("Post", PostSchema);
