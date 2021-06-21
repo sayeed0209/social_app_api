2c2caa7bedf6df255dd8cc795b4272cc2d611d2f
abu sayeed <sayeed0209@gmail.com>
2021-06-21 15:17:34 +0200

    token sent to the client & save to the local storage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+function generateAccessToken(email) {
+	return jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+		expiresIn: "1800s",
+	});
+}
 router.get("/register", (req, res) => {
 	res.render("users/index");
 });
@@ -18,28 +23,23 @@ router.post("/register", async (req, res, next) => {
 });
 
 router.get("/login", async (req, res, next) => {
+	// const users = await User.find({});
+	res.render("users/login");
+});
+router.post("/login", async (req, res, next) => {
 	const user = await User.find({ email: req.body.email });
-	if (!user.length) {
-		res.status(401).send("user not found ");
-	} else {
-		if (await bcrypt.compare(req.body.password, user[0].password)) {
-			// res.send("user found");
-			const token = await jwt.sign(
-				{
-					user: user,
-				},
-				process.env.ACCESS_TOKEN_SECRET,
-				{
-					expiresIn: "1h",
-				}
-			);
-			return res.status(200).json({
-				message: "Auth sucessfull",
-				token: token,
-			});
-		} else {
-			res.status(401).send("user not found ");
-		}
+	console.log(user);
+	if (await bcrypt.compare(req.body.password, user[0].password)) {
+		const token = generateAccessToken({ email: req.body.email });
+		return res.json({ status: "ok", data: token });
 	}
+	res.redirect("/posts");
+
+	// const token = await jwt.sign(
+	// 	{ user: user },
+	// 	process.env.ACCESS_TOKEN_SECRET,
+	// 	{ expiresIn: "1h" }
+	// );
+	// res.send(token);
 });
 module.exports = router;
