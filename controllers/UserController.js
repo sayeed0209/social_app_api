3a1bca9b1fb0035476a33ce669bd774046fe184c
abu sayeed <sayeed0209@gmail.com>
2021-06-25 09:49:22 +0200

    auth for the user  route

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -65,22 +65,13 @@ router.patch("/me", auth, async (req, res) => {
 	const isValidOperation = updates.every(update =>
 		allowedUpdates.includes(update)
 	);
-
 	if (!isValidOperation) {
 		return res.status(400).send({ error: "Invalid updates!" });
 	}
-
 	try {
-		const user = await User.findById(req.user._id);
-
-		updates.forEach(update => (user[update] = req.body[update]));
-		await user.save();
-
-		if (!user) {
-			return res.status(404).send();
-		}
-
-		res.send(user);
+		updates.forEach(update => (req.user[update] = req.body[update]));
+		await req.user.save();
+		res.send(req.user);
 	} catch (e) {
 		res.status(400).send(e);
 	}
